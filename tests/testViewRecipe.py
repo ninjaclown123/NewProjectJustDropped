4724b2ffcacbf38db7fa790e5a01c107b3414438
RaidAldam2000 <al-garafa2000@hotmail.com>
2023-06-09 17:23:40 +0300

    Tester (ViewRecpice) added

--- tests/testViewRecipe.py
+++ tests/testViewRecipe.py
@@ -1,7 +1,26 @@
 import unittest
-import recipeProject
+from Core import recipeProject, Recipe 
 
 class TestRecipeManagement(unittest.TestCase):
+    def setUp(self):
+        self.recip_manager = recipeProject()
+        self.recipe_data = self.recip_manager.data[0]
+        self.recip_obj = Recipe(
+            self.recipe_data['recipeName'],
+            self.recipe_data['recipeAuthor'],
+            self.recipe_data['prepTime'],
+            self.recipe_data['cookTime'],
+            self.recipe_data['servingSize'],
+            self.recipe_data['ingredients'],
+            self.recipe_data['instructions']
+        )
+        self.recipe_str = str(self.recip_obj)
 
     def test_view_recipe(self):
+        expected_str = "Recipe: McBurger\nAuthor: Sam\nPreparation Time: 10 minutes\nCook Time: 12 minutes\nServing Size: 1\nIngredients:\nbun: 1 unit\nsecretPatty: 1 unit\nspecialMayo: 10 grams\nspecialSauce: 20 grams\nlettuce: 8 grams\ntomato: 8 grams\nInstructions:\n1. Assemble the bun and the secret patty.\n2. Spread special mayo and special sauce on the bun.\n3. Add lettuce and tomato on top.\n4. Cook the assembled burger for 12 minutes."
+        self.assertEqual(self.recipe_str, expected_str)
         pass
+
+if __name__ == '__main__':
+    unittest.main()
+    
