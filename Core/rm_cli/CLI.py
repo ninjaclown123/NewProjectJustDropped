3ff1c8403b2086763332678a93b67c1f486f61b2
ninjaclown123 <samsonwilliam12@gmail.com>
2023-06-12 01:40:23 +0300

    made a bunch of changes regrading command line and the 'major oversight'.

--- Core/rm_cli/CLI.py
+++ Core/rm_cli/CLI.py
@@ -1,49 +1,35 @@
-import click
 from Core.recipeProject import RecipeManager
+import cmd
+
 
 rm = RecipeManager()
 
 
-@click.group()
-def cli():
-    pass
+class RmMode(cmd.Cmd):
+    intro = 'Welcome to Recipe Manager!'
+    prompt = 'RmMode> '
 
+    def do_export(self, arg):
+        'Export current recipe data in memory to a JSON file in Core/exports directory. Command: export'
+        rm.exportRecipes()
 
-@click.command()
-def activate():
-    click.echo("Welcome to Recipe Manager>")
-    dic = {'add': rm.addRecipe, 'view': rm.viewRecipe, 'update': rm.updateRecipe, 'export': rm.exportRecipes, 'import': import_recipe}
-    while True:
-        user_input = click.prompt("RmMode>", prompt_suffix=" ")
-        if user_input == "exit":
-            click.echo("Exiting RmMode>")
-            break
-        if user_input not in ['add', 'view', 'update', 'delete', 'export', 'import']:
-            click.echo("Invalid command. Type help for manual.")
-        else:
-            # Process user input or execute the desired commands
-            click.echo(f"You entered: {user_input}")
-            if user_input.startswith('import '):
-                # Parse the command and argument
-                command, arg = user_input.split(' ', 1)
-                # Invoke the command with the argument
-                dic[command](arg)
+    def do_import(self,arg):
+        'Import a JSON file containing recipe data for Recipe Manager. Command: import <filename>'
+        if arg:
+            if arg[-(len(arg)-arg.index('.')):] != '.json':
+                print(str(arg[-(len(arg)-arg.index('.')):]),'files are unacceptable. Please import a valid JSON file.')
             else:
-                dic[user_input]()
+                if arg[-5:] == '.json':
+                    arg = arg[:-5]
 
+                rm.importRecipes(arg)
 
-@click.command()
-@click.argument('filename')
-def import_recipe(filename):
-    click.echo(f"Importing recipes from {filename}...")
+    def do_exit(self,arg):
+        'Exits RmMode shell. Command: exit'
+        print('Thank you for using Recipe Manager!')
 
+        return True
 
-# Define other commands (add_recipe, view_recipe, update_recipe, export_recipes) here
-
-# Add the commands to the CLI
-cli.add_command(activate)
-cli.add_command(import_recipe)
-# Add other commands to the CLI
 
 if __name__ == '__main__':
-    cli()
+    RmMode().cmdloop()
