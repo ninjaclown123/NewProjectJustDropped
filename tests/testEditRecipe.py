4ed4c5a8df8bb68c894a905e0ecccedaf6177798
hamza-ops <60097804@cna-qatar.edu.qa>
2023-06-12 20:23:22 +0300

    new editRecipe implmentation method with new unit tests and integration tests

--- tests/testEditRecipe.py
+++ tests/testEditRecipe.py
@@ -1,54 +1,128 @@
 import unittest
-from Core.recipeProject import RecipeManager
+from Core.recipeProject import RecipeManager, Recipe
 
 
-class TestRecipeManagement(unittest.TestCase):
+class RecipeManagerTests(unittest.TestCase):
+
     def setUp(self):
-        self.rm = RecipeManager()
+        # Create a RecipeManager instance for testing
+        self.recipe_manager = RecipeManager()
 
-    def test_update_recipe_valid_id(self):
-        # Case: Update recipe with valid ID and check if the returned value is True
-        result = self.rm.updateRecipe(
-            id=1,
-            new_recipe_name="Vegan Bolognese",
-            new_recipe_author="Jane Doe",
-            new_prep_time=25,
-            new_cook_time=40,
-            new_serving_size=5
+    def test_editRecipe_existingRecipe(self):
+        # Create a new recipe to be used for editing
+        new_recipe = Recipe(
+            1,
+            "Updated Recipe",
+            "John",
+            30,
+            45,
+            2,
+            [
+                {"ingredientName": "ingredient1", "quantity": 1, "measurement": "unit"},
+                {"ingredientName": "ingredient2", "quantity": 2, "measurement": "units"}
+            ],
+            {"1": "Step 1", "2": "Step 2"}
         )
-        self.assertTrue(result)
 
-        # Verify that the recipe was indeed updated
-        recipe = next((recipe for recipe in self.rm.data if recipe["id"] == 1), None)
-        self.assertIsNotNone(recipe)
-        self.assertEqual(recipe["recipeName"], "Vegan Bolognese")
-        self.assertEqual(recipe["recipeAuthor"], "Jane Doe")
-        self.assertEqual(recipe["prepTime"], 25)
-        self.assertEqual(recipe["cookTime"], 40)
-        self.assertEqual(recipe["servingSize"], 5)
+        # Edit an existing recipe in the RecipeManager's data
+        self.recipe_manager.data = [
+            Recipe(0, "Recipe 1", "Author 1", 10, 20, 3, [], {}),
+            Recipe(1, "Recipe 2", "Author 2", 15, 25, 4, [], {})
+        ]
+        self.recipe_manager.editRecipe(new_recipe)
 
-    def test_update_recipe_invalid_id(self):
-        # Case: Attempt to update recipe with invalid ID and check if the returned value is False
-        result = self.rm.updateRecipe(
-            id=100,  # This ID does not exist
-            new_recipe_name="Vegan Bolognese",
-            new_recipe_author="Jane Doe",
-            new_prep_time=25,
-            new_cook_time=40,
-            new_serving_size=5
+        # Check if the recipe was edited correctly
+        edited_recipe = self.recipe_manager.data[1]
+        self.assertEqual(edited_recipe.id, 1)
+        self.assertEqual(edited_recipe.recipe_name, "Updated Recipe")
+        self.assertEqual(edited_recipe.recipe_author, "John")
+        self.assertEqual(edited_recipe.prep_time, 30)
+        self.assertEqual(edited_recipe.cook_time, 45)
+        self.assertEqual(edited_recipe.serving_size, 2)
+        self.assertEqual(edited_recipe.ingredients, [
+            {"ingredientName": "ingredient1", "quantity": 1, "measurement": "unit"},
+            {"ingredientName": "ingredient2", "quantity": 2, "measurement": "units"}
+        ])
+        self.assertEqual(edited_recipe.instructions, {"1": "Step 1", "2": "Step 2"})
+
+    def test_editRecipe_nonExistingRecipe(self):
+        # Create a new recipe to be used for editing
+        new_recipe = Recipe(
+            3,
+            "New Recipe",
+            "Jane",
+            20,
+            30,
+            1,
+            [
+                {"ingredientName": "ingredient1", "quantity": 1, "measurement": "unit"}
+            ],
+            {"1": "Step 1"}
         )
-        self.assertFalse(result)
 
-    def test_update_recipe_no_changes(self):
-        # Case: Call updateRecipe with valid ID but without providing any new values,
-        # check if the returned value is True and the recipe is unchanged
-        recipe_before_update = next((recipe for recipe in self.rm.data if recipe["id"] == 2), None)
-        result = self.rm.updateRecipe(id=2)
-        self.assertTrue(result)
+        # Edit a non-existing recipe in the RecipeManager's data
+        self.recipe_manager.data = [
+            Recipe(0, "Recipe 1", "Author 1", 10, 20, 3, [], {}),
+            Recipe(1, "Recipe 2", "Author 2", 15, 25, 4, [], {})
+        ]
 
-        # Verify that the recipe was not changed
-        recipe_after_update = next((recipe for recipe in self.rm.data if recipe["id"] == 2), None)
-        self.assertEqual(recipe_before_update, recipe_after_update)
+        # Use assertRaises to check if ValueError is raised
+        with self.assertRaises(ValueError):
+            self.recipe_manager.editRecipe(new_recipe)
+
+        # Check that the data remains unchanged
+        self.assertEqual(len(self.recipe_manager.data), 2)
+
+    def test_editRecipe_noChanges(self):
+        # Create a new recipe to be used for editing
+        new_recipe = Recipe(
+            1,
+            "Recipe 1",
+            "Author 1",
+            10,
+            20,
+            2,
+            [],
+            {}
+        )
+
+        # Add the recipe to the data
+        self.recipe_manager.data = [
+            new_recipe,
+            Recipe(1, "Recipe 2", "Author 2", 15, 25, 4, [], {})
+        ]
+
+        self.recipe_manager.editRecipe(new_recipe)
+
+        # Check that the data remains unchanged
+        self.assertEqual(len(self.recipe_manager.data), 2)
+        self.assertEqual(self.recipe_manager.data[0], new_recipe)
+
+    def test_editRecipe_emptyData(self):
+        # Create a new recipe to be used for editing
+        new_recipe = Recipe(
+            1,
+            "Updated Recipe",
+            "John",
+            30,
+            45,
+            2,
+            [
+                {"ingredientName": "ingredient1", "quantity": 1, "measurement": "unit"},
+                {"ingredientName": "ingredient2", "quantity": 2, "measurement": "units"}
+            ],
+            {"1": "Step 1", "2": "Step 2"}
+        )
+
+        # Edit a recipe when the data is empty
+        self.recipe_manager.data = []
+
+        # Use assertRaises to check if ValueError is raised
+        with self.assertRaises(ValueError):
+            self.recipe_manager.editRecipe(new_recipe)
+
+        # Check that the data remains empty
+        self.assertEqual(len(self.recipe_manager.data), 0)
 
 
 if __name__ == '__main__':
