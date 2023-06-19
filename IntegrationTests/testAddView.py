2416e5c46dceec5548779a29c66cb46afd0e1930
RaidAldam2000 <al-garafa2000@hotmail.com>
2023-06-19 19:15:08 +0300

    test add view integration it should work now

--- IntegrationTests/testAddView.py
+++ IntegrationTests/testAddView.py
@@ -1,11 +1,8 @@
 import unittest
-
-import unittest
 from Core.recipeProject import RecipeManager, Recipe
 
 
 class TestRecipeManagerIntegration(unittest.TestCase):
-
 
     def test_addRecipe_and_viewRecipe(self):
         rm = RecipeManager()
@@ -31,7 +28,7 @@ class TestRecipeManagerIntegration(unittest.TestCase):
         recipe_list = rm.viewRecipeList()
 
         self.assertNotIn(new_recipe, recipe_list)
-        self.assertEqual(len(recipe_list),1)
+        self.assertEqual(len(recipe_list), 1)
         self.assertEqual(recipe_list[0].id, 3)
         self.assertEqual(recipe_list[0].recipeName, "twester")
         self.assertEqual(recipe_list[0].recipeAuthor, "Raid")
@@ -39,15 +36,13 @@ class TestRecipeManagerIntegration(unittest.TestCase):
         self.assertEqual(recipe_list[0].cookTime, 50)
         self.assertEqual(recipe_list[0].servingSize, 2)
         self.assertEqual(recipe_list[0].ingredients, [
-
             {"ingredientName": "spicy fries", "quantity": 3, "measurement": "pieces"},
             {"ingredientName": "pepsi", "quantity": 1, "measurement": "cup"},
             {"ingredientName": "sos", "quantity": 1, "measurement": "sosCup"},
         ])
-        
-        self.assertEqual(recipe_list[0].istructions, {
-            "1":  "Add the sos.",
-            "2": "Add the spicy fries"
+        self.assertEqual(recipe_list[0].instructions, {
+            "1": "Add the sos.",
+            "2": "Add the spicy fries with pepsi."
         })
 
         self.assertIn(new_recipe, rm.data)
