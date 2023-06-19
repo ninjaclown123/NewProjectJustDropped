6d069b7aa64afbcf40755d9bf1b3b5f9631e506a
hamza-ops <60097804@cna-qatar.edu.qa>
2023-06-19 10:05:17 +0300

    implemented edit_id function and edit.html

--- Core/app.py
+++ Core/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import json
+
+from flask import Flask, render_template, redirect, url_for, flash, request
 from Core.recipeProject import RecipeManager
 import logging
 
@@ -35,11 +37,72 @@ def view_id(id):
 @app.route('/add')
 def add():
     return render_template('add.html',recipe = {'Recipe Name':None, 'Recipe Author':None, 'Preparation time':None,'Cook time':None,'Serving Size':None,'Ingredients':[], 'Instructions':{}})
-    
 
-@app.route('/edit/<id>')
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit_id(id):
-    return render_template('edit.html',id = id)
+    # Find the recipe with the given id in the recipe list
+    recipe = None
+    for r in rm.RecipeList():
+        if r.id == id:
+            recipe = r
+            break
+
+    if recipe is None:
+        # Handle the case when the recipe is not found
+        flash("Recipe not found.")
+        return redirect(url_for('index'))
+
+    if request.method == 'POST':
+        # Update the recipe with the submitted form data
+        recipe.recipe_name = request.form.get('recipe_name')
+        recipe.recipe_author = request.form.get('recipe_author')
+        recipe.prep_time = request.form.get('prep_time')
+        recipe.cook_time = request.form.get('cook_time')
+        recipe.serving_size = request.form.get('serving_size')
+
+        # Debugging: Print the form inputs
+        print("Recipe Name:", recipe.recipe_name)
+        print("Author:", recipe.recipe_author)
+        print("Preparation Time:", recipe.prep_time)
+        print("Cook Time:", recipe.cook_time)
+        print("Serving Size:", recipe.serving_size)
+
+        # Update ingredients
+        ingredients = []
+        i = 0
+        while f'ingredient_name_{i}' in request.form:
+            ingredient_name = request.form.get(f'ingredient_name_{i}')
+            ingredient_quantity = request.form.get(f'ingredient_quantity_{i}')
+            ingredient_measurement = request.form.get(f'ingredient_measurement_{i}')
+            ingredients.append({
+                "ingredientName": ingredient_name,
+                "quantity": ingredient_quantity,
+                "measurement": ingredient_measurement
+            })
+            i += 1
+
+        recipe.ingredients = ingredients
+
+        # Debugging: Print the ingredients
+        print("Ingredients:", recipe.ingredients)
+
+        # Update instructions
+        instructions = {}
+        i = 0
+        while f'instruction_{i}' in request.form:
+            instruction = request.form[f'instruction_{i}']
+            instructions[i + 1] = instruction
+            i += 1
+        recipe.instructions = instructions
+
+        # Debugging: Print the instructions
+        print("Instructions:", recipe.instructions)
+
+        return redirect(url_for('view_id', id=id))
+
+    return render_template('edit.html', recipe=recipe)
+
 
 @app.route('/delete/<id>')
 def delete_id(id):
